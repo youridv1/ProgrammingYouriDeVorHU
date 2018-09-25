7c4131066fec8ecd8a3f21dba369b309cf53f7e3
Youri de Vor <youri.devor@student.hu.nl>
2018-09-25 19:23:33 +0200

    les 6 NS werkend

---
+++ venv/Les6/6_NS.py
@@ -0,0 +1,28 @@
+def standaardprijs(afstandKM):
+    if afstandKM > 50:
+        res = 15 + afstandKM*0.6
+    else:
+        res = afstandKM*0.8
+    if res < 0:
+        res = 0
+    return res
+
+
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    res1 = standaardprijs(afstandKM)
+    if weekendrit == False and leeftijd < 12 or leeftijd >= 65 and weekendrit == False:
+        res0 = res1 * 0.70
+    else:
+        if weekendrit == True and leeftijd < 12 or leeftijd >= 65 and weekendrit == True:
+            res0 = res1 * 0.65
+        else:
+            if weekendrit == True and leeftijd >= 12 and leeftijd < 65:
+                res0 = res1 * 0.60
+            else:
+                res0 = res1
+
+    print(res0)
+
+
+
+
