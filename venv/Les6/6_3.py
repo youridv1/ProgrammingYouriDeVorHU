413006094e0b55da61281e3c150b4af34f4b524e
Youri de Vor <youri.devor@student.hu.nl>
2018-09-27 16:13:32 +0200

    6_3 aanpassing

--- venv/Les6/6_3.py
+++ venv/Les6/6_3.py
@@ -1,5 +1,5 @@
-def lang_genoeg(a):
-    if a >= 120:
+def lang_genoeg(lengte):
+    if lengte >= 120:
         print("Je bent lang genoeg voor de attractie!")
     else:
         print("Sorry, je bent te klein!")
