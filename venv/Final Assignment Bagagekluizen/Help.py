6581ba890ce4c99d1c163aebacdf428c0b78df69
Youri de Vor <youri.devor@student.hu.nl>
2018-10-03 20:35:19 +0200

    kluizen af

--- venv/Final Assignment Bagagekluizen/Help.py
+++ venv/Final Assignment Bagagekluizen/Help.py
@@ -15,6 +15,66 @@ def nieuwe_kluis():
     kluizenlines = kluizen.readlines()
     for line in kluizenlines:
         line = line.split(';')
+        gevondenkluizen.append(int(line[0]))
+    kluizenlist = [x for x in kluizenlist if x not in gevondenkluizen]
+    kluizen.close()
+    if len(kluizenlist) > 0:
+        code = input("Voer een toegangscode van minimaal 4 tekens in voor het later openen van de kluis: ")
+    else:
+        print("Sorry, alle kluizen zijn in gebruik.")
+    if len(code) >= 4:
+        kluizen = open('kluizen.txt', 'a')
+        kluizen.write(str(min(kluizenlist)) + ';' + str(code) + '\n')
+        print('U hebt kluis nummer ' + str(min(kluizenlist)))
+    else:
+        print("Sorry, die code is niet lang genoeg.")
+
+def kluis_openen():
+    nummer = input("Geef uw kluisnummer: ")
+    code = input("Geef uw code: ")
+    kluizenlist = []
+    wachtwoorden = []
+    kluizen = open('kluizen.txt')
+    kluizenlines = kluizen.readlines()
+    kluizen.close()
+    for line in kluizenlines:
+        line = line.split(';')
+        line[1] = line[1].strip()
+        kluizenlist.append(line[0])
+        wachtwoorden.append(line[1])
+    if nummer in kluizenlist and code in wachtwoorden:
+        print("Kluis nummer " + str(nummer) + " is nu open.")
+    else:
+        print("Kluisnummer en/of code incorrect.")
+
+def kluis_teruggeven():
+    nummer = input("Geef uw kluisnummer: ")
+    code = input("Geef uw code: ")
+    kluizenlist = []
+    wachtwoorden = []
+    kluizen = open('kluizen.txt')
+    kluizenlines = kluizen.readlines()
+    kluizen.close()
+    for line in kluizenlines:
+        line = line.split(';')
+        line[1] = line[1].strip()
+        kluizenlist.append(line[0])
+        wachtwoorden.append(line[1])
+    if nummer in kluizenlist and code in wachtwoorden:
+        combinatie = [nummer + ';' + code + '\n']
+        kluizenlines = [x for x in kluizenlines if x not in combinatie]
+        kluizen = open('kluizen.txt', 'w')
+        for i in kluizenlines:
+            kluizen.write(i)
+        print("De kluis is nu weer vrij.")
+    else:
+        print("Kluisnummer en/of code incorrect.")
+
+
+
+
+
+
 
 
 
